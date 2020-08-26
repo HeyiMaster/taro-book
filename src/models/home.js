6c99d191503256198229af38ee8d772058f5af89
合一 <1255968521@qq.com>
2020-08-26 18:47:13 +0800

    release: v1.0 h5, weapp, rn

--- src/models/home.js
+++ src/models/home.js
@@ -4,18 +4,66 @@ export default {
   namespace: 'home',
   state: {
     list: [],
-    banner: []
+    banner: [],
   },
   effects: {
     *fetchBanner(_, { call, put }) {
-      const { data } = yield call(homeService.fetchBanner);
+      // const { data } = yield call(homeService.fetchBanner);
+      const data = [
+        {
+          url:
+            'http://walker-markdown.oss-cn-shenzhen.aliyuncs.com/uPic/milada-vigerova-p8Drpg_duLw-unsplash.jpg',
+        },
+        {
+          url:
+            'http://walker-markdown.oss-cn-shenzhen.aliyuncs.com/uPic/cristofer-jeschke-AqLIkOzWDAk-unsplash.jpg',
+        },
+      ];
       yield put({
         type: 'save',
         banner: data,
       });
     },
     *fetchList(_, { call, put }) {
-      const { data } = yield call(homeService.fetchList);
+      // const { data } = yield call(homeService.fetchList);
+      const data = [
+        {
+          title: '美丽的商品',
+          desc: '商品描述111',
+          url:
+            'http://walker-markdown.oss-cn-shenzhen.aliyuncs.com/uPic/milada-vigerova-p8Drpg_duLw-unsplash.jpg',
+        },
+        {
+          title: '漂亮的商品',
+          desc: '商品描述111',
+          url:
+            'http://walker-markdown.oss-cn-shenzhen.aliyuncs.com/uPic/cristofer-jeschke-AqLIkOzWDAk-unsplash.jpg',
+        },
+        {
+          title: '善良的商品',
+          desc: '商品描述111',
+          url:
+            'http://walker-markdown.oss-cn-shenzhen.aliyuncs.com/uPic/milada-vigerova-p8Drpg_duLw-unsplash.jpg',
+        },
+        {
+          title: '好看的商品',
+          desc: '商品描述111',
+          url:
+            'http://walker-markdown.oss-cn-shenzhen.aliyuncs.com/uPic/cristofer-jeschke-AqLIkOzWDAk-unsplash.jpg',
+        },
+        {
+          title: '好用的商品',
+          desc: '商品描述111',
+          url:
+            'http://walker-markdown.oss-cn-shenzhen.aliyuncs.com/uPic/milada-vigerova-p8Drpg_duLw-unsplash.jpg',
+        },
+        {
+          title: '好喜欢的商品',
+          desc: '商品描述111',
+          url:
+            'http://walker-markdown.oss-cn-shenzhen.aliyuncs.com/uPic/cristofer-jeschke-AqLIkOzWDAk-unsplash.jpg',
+        },
+      ];
       yield put({
         type: 'save',
         list: data,
@@ -31,4 +79,3 @@ export default {
     },
   },
 };
-
